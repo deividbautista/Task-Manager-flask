d3a8bf69e86f31f2a4436c7d67539bcef2098866
deivid <101598816+deividbautista@users.noreply.github.com>
2023-08-10 13:50:10 -0500

    Update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from random import sample
 import os
 import database as db
 
@@ -8,6 +9,21 @@ template_dir = os.path.join(template_dir, 'src', 'templates')
 app = Flask(__name__, template_folder = template_dir)
 
 
+def idAleatorio():
+    # Caracteres que puede poseer el string aleatorio.
+    id_aleatorio = "0123456789"
+    # Logintud determinada para el estring aleatorio.
+    longitud         = 10
+    # Utilizamos la función upper para combertir los caracteres en mayuscula.
+    secuencia        = id_aleatorio.upper()
+    # Definimos la variable "resultado_aleatorio", que con la función sample 
+    # y los parametros de secuencia y longitud, generamos el string aleatorio.
+    resultado_aleatorio  = sample(secuencia, longitud)
+    # Volvemos a definir a la varibale "id_aleatorio", pero esta vez le 
+    # concatenamos el valor optenido de "resultado_aleatorio". 
+    id_aleatorio     = "".join(resultado_aleatorio)
+    # Finalmente retornamos de nuestra función el resultado con el string aleatorio.
+    return id_aleatorio
 
 def datosUsuarios():
     cursor = db.database.cursor()
@@ -21,7 +37,6 @@ def datosUsuarios():
     cursor.close()  
 
     dataUser=insertObject  
-
     return dataUser
 
 
@@ -29,7 +44,15 @@ def datosUsuarios():
 @app.route('/')
 def home():
     cursor = db.database.cursor()
-    cursor.execute("SELECT * FROM procesos")
+    # cursor.execute("SELECT * FROM procesos")
+    query = """
+    SELECT p.*, GROUP_CONCAT(u.fullname) AS users
+    FROM procesos p
+    LEFT JOIN asignaciones a ON p.id_proceso = a.id_proceso
+    LEFT JOIN users u ON a.id = u.id
+    GROUP BY p.id_proceso
+    """
+    cursor.execute(query)
     myresult = cursor.fetchall()
     #Convertir los datos a diccionario
     insertObject = []
@@ -38,7 +61,9 @@ def home():
         insertObject.append(dict(zip(columnNames, record)))
     cursor.close()   
     dataUser = datosUsuarios()
+    
     return render_template('index.html', data=insertObject, datosU=dataUser )
+
 
 
 #Ruta para guardar usuarios en la Base de datos
@@ -47,20 +72,30 @@ def addUser():
     Titulo = request.form['Titulo']
     Descripcion = request.form['Descripcion']
     Fecha = request.form['fecha']
+    id_proceso = idAleatorio()
+    asignados = request.form.getlist('usuarios_seleccionados')
 
     if Titulo and Descripcion:
         cursor = db.database.cursor()
-        sql = "INSERT INTO procesos (Titulo, Descripcion, Fecha_terminación) VALUES (%s, %s, %s)"
-        data = (Titulo, Descripcion, Fecha)
+        sql = "INSERT INTO procesos (id_proceso, Titulo, Descripcion, Fecha_terminación) VALUES (%s, %s, %s, %s)"
+        data = (id_proceso, Titulo, Descripcion, Fecha)
         cursor.execute(sql, data)
         db.database.commit()
+
+    if len (asignados) > 0:
+        for Numid in asignados:
+            cursor = db.database.cursor()
+            sql = "INSERT INTO asignaciones (id, id_proceso) VALUES (%s, %s)"
+            data = (Numid, id_proceso)
+            cursor.execute(sql, data)
+            db.database.commit()
+            print(Numid)
     return redirect(url_for('home'))
 
 
 
 @app.route('/delete/<string:idP>')
 def delete(idP):
-    print(idP)
     dato = (idP)
     cursor = db.database.cursor()
     sql ="DELETE FROM procesos WHERE id_proceso= {}"
--- src/static/js/procesos.js
+++ src/static/js/procesos.js
@@ -1,5 +1,22 @@
+// Obtén todos los labels de los usuarios
+const userLabels = document.querySelectorAll(".dropdown-menu li label");
+const idlabel = document.querySelectorAll(".usuariosSeleccionados");
+const boton  = document.getElementById("boton");
+const checkboxes = document.querySelectorAll(".usuariosSelect");
+
+
+// Agrega un evento click a cada label.
+userLabels.forEach(label => {
+  label.addEventListener('click', (event) => {
+    // Prevenir que se cierre la lista.
+    event.stopPropagation();
+  });
+});
+
+// Función para obtener la fecha del dia de hoy y asignarla automaticament en el campo de periodo.
 window.onload = function() {
-    var dateField = document.getElementById("fecha");
-    var today = new Date().toISOString().split("T")[0];
-    dateField.value = today;
-  };
+  var dateField = document.getElementById("fecha");
+  var today = new Date().toISOString().split("T")[0];
+  dateField.value = today;
+};
+
